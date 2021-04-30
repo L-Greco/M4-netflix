22744efafe4247918bbdcfcda899d4446463174c
L-Greco <makaronaskonstandinos@gmail.com>
2021-04-30 17:09:57 +0300

    no film found!

--- m4-netflix/src/components/Display.jsx
+++ m4-netflix/src/components/Display.jsx
@@ -1,41 +1,32 @@
-import React from "react"
-import { Container, Row} from "react-bootstrap"
-
+import React from "react";
+import { Card, Row } from "react-bootstrap";
 
 class Display extends React.Component {
-
-
-
-    render() {
-
-        return <>
-           <div className="container-fluid">
-            <Row>
-            {console.log(this.props.selected)}
-
-                {
-                    
-                    this.props.selected.map((film) => {
-                        
-                        return (<div key={film.imdbID} className="col-md-2 m-1">
-                                <span class="text-truncate">{film.title}</span>
-                                <img height="250px" alt={film.title} className="d-block w-80" src={film.Poster} />
-                                <p></p>
-                                </div>)
-
-                        
-                                    
-                                    })
-
-
-                }
-
-
-            </Row>
-            </div>
-
-        </>
-    }
+  render() {
+    return (
+      <>
+        <div className="container-fluid">
+          <Row>
+            {this.props.queryError && <h1>No film found!! </h1>}
+            {this.props.selected.map((film) => {
+              return (
+                <div key={film.imdbID} className="col-md-2 m-1">
+                  <span class="text-truncate">{film.title}</span>
+                  <img
+                    height="250px"
+                    alt={film.title}
+                    className="d-block w-80"
+                    src={film.Poster}
+                  />
+                  <p></p>
+                </div>
+              );
+            })}
+          </Row>
+        </div>
+      </>
+    );
+  }
 }
 
-export default Display
+export default Display;
